dc7e39e25577ff8c6bf3b30123c52d0aa391f5fa
GILVANROCHA <gilvanrocha@gmail.com>
2014-04-11 21:08:55 -0300

    --force

--- projetoFP/settings.py
+++ projetoFP/settings.py
@@ -36,7 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+    'core',
+    'pessoas',
+    'caixas',
 )
 
 MIDDLEWARE_CLASSES = (
